414f91438cb61809ed1a969597607f4213dc7562
Saurav Kumar <156607538+SauravKumar2201@users.noreply.github.com>
2024-02-10 11:15:12 +0530

    Binary search.cpp

---
+++ Binary search/binary search.cpp
@@ -0,0 +1,11 @@
+int search(vector<int> &arr, int target) {
+    int n= arr.size();
+    int low =0, high= n-1;
+    while(low<= high){
+        int mid= low+ (high-low)/2;
+        if(arr[mid]==target) return mid;
+        else if(target>arr[mid]) low = mid+1;
+        else high= mid-1;
+    }
+    return -1;
+}
